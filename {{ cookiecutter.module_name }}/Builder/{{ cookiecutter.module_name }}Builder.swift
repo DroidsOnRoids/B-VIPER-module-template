b85ae1f93020f2340c46f08b58394db9ee8465bc
Bartosz Górka <bartosz.gorka@droidsonroids.pl>
2017-08-03 13:14:03 +0200

    changed var_name attribute

--- {{ cookiecutter.module_name }}/Builder/{{ cookiecutter.module_name }}Builder.swift
+++ {{ cookiecutter.module_name }}/Builder/{{ cookiecutter.module_name }}Builder.swift
@@ -13,12 +13,12 @@ protocol {{ cookiecutter.module_name }}BuilderProtocol {}
 final class {{ cookiecutter.module_name }}Builder: {{ cookiecutter.module_name }}BuilderProtocol {
     
     func build{{ cookiecutter.module_name }}Module() -> UIViewController {
-        let {{ cookiecutter.var_name }}ViewController = {{ cookiecutter.module_name }}ViewController(searchType: .findJobs)
-        let {{ cookiecutter.var_name }}Router = {{ cookiecutter.module_name }}Router(viewController: {{ cookiecutter.var_name }}ViewController)
-        let {{ cookiecutter.var_name }}Presenter = {{ cookiecutter.module_name }}Presenter(interactor: {{ cookiecutter.module_name }}Interactor(), router: {{ cookiecutter.var_name }}Router, view: {{ cookiecutter.var_name }}ViewController)
+        let {{ cookiecutter.module_var_name }}ViewController = {{ cookiecutter.module_name }}ViewController(searchType: .findJobs)
+        let {{ cookiecutter.module_var_name }}Router = {{ cookiecutter.module_name }}Router(viewController: {{ cookiecutter.module_var_name }}ViewController)
+        let {{ cookiecutter.module_var_name }}Presenter = {{ cookiecutter.module_name }}Presenter(interactor: {{ cookiecutter.module_name }}Interactor(), router: {{ cookiecutter.module_var_name }}Router, view: {{ cookiecutter.module_var_name }}ViewController)
         
-        {{ cookiecutter.var_name }}ViewController.presenter = {{ cookiecutter.var_name }}Presenter
+        {{ cookiecutter.module_var_name }}ViewController.presenter = {{ cookiecutter.module_var_name }}Presenter
         
-        return {{ cookiecutter.var_name }}ViewController
+        return {{ cookiecutter.module_var_name }}ViewController
     }
 }
